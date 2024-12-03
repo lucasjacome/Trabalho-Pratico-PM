430b88ace1453eb41ce8479fb800dc5e10958286
arthurhasantos <arthurhsantos2020@gmail.com>
2024-12-03 19:57:02 -0300

    Correção na classe StatusPassagem (ajustes de lógica)

--- codigo/src/Entidades/StatusPassagem.java
+++ codigo/src/Entidades/StatusPassagem.java
@@ -1,9 +1,19 @@
 package Entidades;
 
 public enum StatusPassagem {
-    PASSAGEM_ADQUIRIDA,
-    PASSAGEM_CANCELADA,
-    CHECK_IN_REALIZADO,
-    EMBARQUE_REALIZADO,
-    NO_SHOW;
+    PASSAGEM_ADQUIRIDA("Passagem adquirida"),
+    PASSAGEM_CANCELADA("Passagem cancelada"),
+    CHECK_IN_REALIZADO("Check-in realizado"),
+    EMBARQUE_REALIZADO("Embarque realizado"),
+    NO_SHOW("No-show");
+
+    private final String descricao;
+
+    StatusPassagem(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
