6ff0c86e345c49bb958cc96a7950b4f800879eba
arthurhasantos <arthurhsantos2020@gmail.com>
2024-12-03 19:55:23 -0300

    Correção da classe PassageiroEmbarque (gerenciamento de embarque)

--- codigo/src/Entidades/PassageiroEmbarque.java
+++ codigo/src/Entidades/PassageiroEmbarque.java
@@ -1,12 +1,17 @@
 package Entidades;
 
+import java.util.Objects;
+
 public class PassageiroEmbarque {
     private Passageiro passageiro;
     private boolean embarcado;
 
     public PassageiroEmbarque(Passageiro passageiro) {
+        if (passageiro == null) {
+            throw new IllegalArgumentException("Passageiro não pode ser nulo.");
+        }
         this.passageiro = passageiro;
-        this.embarcado = false; // Inicialmente não embarcado
+        this.embarcado = false;
     }
 
     public Passageiro getPassageiro() {
@@ -19,5 +24,25 @@ public class PassageiroEmbarque {
 
     public void setEmbarcado(boolean embarcado) {
         this.embarcado = embarcado;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Passageiro: %s, Embarcado: %s", passageiro, embarcado ? "Sim" : "Não");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        PassageiroEmbarque that = (PassageiroEmbarque) obj;
+        return Objects.equals(passageiro, that.passageiro);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passageiro);
     }
 }
