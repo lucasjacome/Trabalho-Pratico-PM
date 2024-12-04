1df79048ffd67f3acb223d1634a7f50f618a979c
arthurhasantos <arthurhsantos2020@gmail.com>
2024-12-03 23:12:07 -0300

    Adiciona testes para Aeroporto com coordenadas geográficas

--- codigo/src/Tests/AeroportoTest.java
+++ codigo/src/Tests/AeroportoTest.java
@@ -8,36 +8,44 @@ class AeroportoTest {
 
     @Test
     void testAeroportoConstrutorValido() {
-        Aeroporto aeroporto = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil");
+        Aeroporto aeroporto = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil", -23.5505,
+                -46.6333);
         assertEquals("Aeroporto Internacional", aeroporto.getNome());
         assertEquals("GRU", aeroporto.getSigla());
         assertEquals("São Paulo", aeroporto.getCidade());
         assertEquals("SP", aeroporto.getEstado());
         assertEquals("Brasil", aeroporto.getPais());
+        assertEquals(-23.5505, aeroporto.getLatitude(), 0.0001);
+        assertEquals(-46.6333, aeroporto.getLongitude(), 0.0001);
     }
 
     @Test
     void testAeroportoConstrutorInvalido() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, 
-            () -> new Aeroporto("Aeroporto Internacional", "GR", "São Paulo", "SP", "Brasil"));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> new Aeroporto("Aeroporto Internacional", "GR", "São Paulo", "SP", "Brasil", -23.5505, -46.6333));
         assertEquals("A sigla deve conter exatamente 3 caracteres.", exception.getMessage());
     }
 
     @Test
     void testAeroportoConstrutorValoresPadrao() {
-        Aeroporto aeroporto = new Aeroporto("", "GRU", "", "", "");
+        Aeroporto aeroporto = new Aeroporto("", "GRU", "", "", "", 0.0, 0.0);
         assertEquals("Não definido", aeroporto.getNome());
         assertEquals("GRU", aeroporto.getSigla());
         assertEquals("Não definida", aeroporto.getCidade());
         assertEquals("Não definido", aeroporto.getEstado());
         assertEquals("Não definido", aeroporto.getPais());
+        assertEquals(0.0, aeroporto.getLatitude(), 0.0001);
+        assertEquals(0.0, aeroporto.getLongitude(), 0.0001);
     }
 
     @Test
     void testEqualsAndHashCode() {
-        Aeroporto aeroporto1 = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil");
-        Aeroporto aeroporto2 = new Aeroporto("Aeroporto Nacional", "GRU", "Guarulhos", "SP", "Brasil");
-        Aeroporto aeroporto3 = new Aeroporto("Aeroporto do Rio", "GIG", "Rio de Janeiro", "RJ", "Brasil");
+        Aeroporto aeroporto1 = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil", -23.5505,
+                -46.6333);
+        Aeroporto aeroporto2 = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil", -23.5505,
+                -46.6333);
+        Aeroporto aeroporto3 = new Aeroporto("Aeroporto do Rio", "GIG", "Rio de Janeiro", "RJ", "Brasil", -22.9068,
+                -43.1729);
 
         assertEquals(aeroporto1, aeroporto2);
         assertNotEquals(aeroporto1, aeroporto3);
@@ -47,7 +55,10 @@ class AeroportoTest {
 
     @Test
     void testToString() {
-        Aeroporto aeroporto = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil");
-        assertEquals("Aeroporto Internacional (GRU), São Paulo, SP, Brasil", aeroporto.toString());
+        Aeroporto aeroporto = new Aeroporto("Aeroporto Internacional", "GRU", "São Paulo", "SP", "Brasil", -23.5505,
+                -46.6333);
+        assertEquals(
+                "Aeroporto Internacional (GRU), São Paulo, SP, Brasil - Latitude: -23.550500, Longitude: -46.633300",
+                aeroporto.toString());
     }
 }
