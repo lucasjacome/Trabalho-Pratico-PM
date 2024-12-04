c16491941457ba6d6dd041f0112ed0f3ad32c932
arthurhasantos <arthurhsantos2020@gmail.com>
2024-12-03 23:12:20 -0300

    Corrige testes de AeroportoManager para considerar coordenadas geográficas

--- codigo/src/Tests/AeroportoManagerTest.java
+++ codigo/src/Tests/AeroportoManagerTest.java
@@ -3,7 +3,6 @@ package Tests;
 import Entidades.Aeroporto;
 import Entidades.AeroportoManager;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AeroportoManagerTest {
@@ -11,7 +10,7 @@ class AeroportoManagerTest {
     @Test
     void testAdicionarAeroporto() {
         AeroportoManager manager = new AeroportoManager();
-        Aeroporto aeroporto = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil");
+        Aeroporto aeroporto = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil", -23.5505, -46.6333);
 
         assertTrue(manager.adicionarAeroporto(aeroporto));
         assertFalse(manager.adicionarAeroporto(aeroporto));
@@ -20,8 +19,9 @@ class AeroportoManagerTest {
     @Test
     void testListarAeroportos() {
         AeroportoManager manager = new AeroportoManager();
-        Aeroporto aeroporto1 = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil");
-        Aeroporto aeroporto2 = new Aeroporto("Aeroporto RJ", "GIG", "Rio de Janeiro", "RJ", "Brasil");
+        Aeroporto aeroporto1 = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil", -23.5505, -46.6333);
+        Aeroporto aeroporto2 = new Aeroporto("Aeroporto RJ", "GIG", "Rio de Janeiro", "RJ", "Brasil", -22.9094,
+                -43.1737);
 
         manager.adicionarAeroporto(aeroporto1);
         manager.adicionarAeroporto(aeroporto2);
@@ -32,7 +32,7 @@ class AeroportoManagerTest {
     @Test
     void testBuscarAeroportoPorSigla() {
         AeroportoManager manager = new AeroportoManager();
-        Aeroporto aeroporto = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil");
+        Aeroporto aeroporto = new Aeroporto("Aeroporto SP", "GRU", "São Paulo", "SP", "Brasil", -23.5505, -46.6333);
 
         manager.adicionarAeroporto(aeroporto);
 
